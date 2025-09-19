4af2b92b478ec220c7ff608faa22e282d435543a
Devashish Priyadarshi <devashish234073@gmail.com>
2025-09-20 01:23:03 +0530

    added a bat file that will switch to each of the 10 branches and stay for 10 seconds

---
+++ switch-branches-demo.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM List of branches to switch
+set branches=torus-following-hand-step1 torus-following-hand-step2 torus-following-hand-step3 torus-following-hand-step4 torus-following-hand-step5 torus-following-hand-step6 torus-following-hand-step7 torus-following-hand-step8 torus-following-hand-step9 torus-following-hand-step10
+
+for %%B in (%branches%) do (
+    echo Switching to branch %%B
+    git checkout %%B
+    echo Staying on %%B for 10 seconds...
+    timeout /t 10 /nobreak
+)
+
+echo Done switching branches.
